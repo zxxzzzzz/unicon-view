4975af8a3df03d2d812a481995881c9878f7bf75
zxx <xin.zhang@robotrak.cn>
2022-08-26 13:36:52 +0800

    wip: 设置端口参数

--- src/views/union/component/setPortParamModal.vue
+++ src/views/union/component/setPortParamModal.vue
@@ -1,5 +1,10 @@
 <template>
   <Modal v-model:visible="visible" title="设置端口参数" @ok="handleOk">
+    <Tabs>
+      <template v-for="port in portList" :key="port">
+        <TabPane :tab="port" />
+      </template>
+    </Tabs>
     <div>
       <Form>
         <FormItem label="" />
@@ -8,10 +13,14 @@
   </Modal>
 </template>
 <script lang="ts" setup>
-  import { Modal, Form, FormItem } from 'ant-design-vue';
-  import { ref } from 'vue';
+  import { Modal, Form, FormItem, Tabs, TabPane } from 'ant-design-vue';
+  import cytoscape from 'cytoscape';
+  import { computed, ref } from 'vue';
   const visible = ref(false);
-
+  const props = defineProps<{ node: cytoscape.CollectionReturnValue }>();
+  const portList = computed(() => {
+    return props.node.data('portList') as string[];
+  });
   const handleOk = () => {};
   defineExpose({
     open() {
