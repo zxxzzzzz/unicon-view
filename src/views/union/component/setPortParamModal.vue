7d1642ee3c165ad7580b8f622f081005bc2ae6cd
zxx <xin.zhang@robotrak.cn>
2022-10-08 17:32:22 +0800

    feat: 新的编辑框

--- src/views/union/component/setPortParamModal.vue
+++ src/views/union/component/setPortParamModal.vue
@@ -1,6 +1,79 @@
 <template>
-  <Modal v-model:visible="visible" title="设置端口参数" @ok="handleOk">
-    <Card class="h-100 overflow-auto">
+  <Modal v-model:visible="visible" @ok="handleOk" :closable="false" :footer="null" width="800px">
+    <div>
+      <div class="flex bg-[rgb(240,240,240)]">
+        <template v-for="(port, index) in portList" :key="port">
+          <div class="p-1 border">{{ port }}</div>
+        </template>
+      </div>
+      <div class="main flex">
+        <div class="bg-[rgb(240,240,240)] w-20">
+          <div class="h-16 justify-center items-center flex">
+            <div>同步以太</div>
+          </div>
+          <div class="h-16 justify-center items-center flex">
+            <div>1588</div>
+          </div>
+        </div>
+        <div class="content m-2 bg-[rgb(240,240,240)] w-full p-4">
+          <div>
+            <Form :labelCol="{ span: 8 }" labelAlign="left">
+              <div class="flex flex-wrap justify-between">
+                <FormItem label="端口名称" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].name" />
+                </FormItem>
+                <FormItem label="框-槽位-端口号" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].aliasName" />
+                </FormItem>
+                <FormItem label="端口状态" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].state" />
+                </FormItem>
+                <FormItem label="使能状态" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].enableState" />
+                </FormItem>
+                <FormItem label="时钟id" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].ptpClockId" />
+                </FormItem>
+                <FormItem label="域号" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].ptpDomain" />
+                </FormItem>
+                <FormItem label="时钟等级" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].ptpClockClass" />
+                </FormItem>
+                <FormItem label="优先级1" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].ptpPriority1" />
+                </FormItem>
+                <FormItem label="优先级2" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].ptpPriority2" />
+                </FormItem>
+                <FormItem label="时间源" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].timeSource" />
+                </FormItem>
+                <FormItem label="通知间隔" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].announceIntv" />
+                </FormItem>
+                <FormItem label="同步间隔" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].syncIntv" />
+                </FormItem>
+                <FormItem label="延时间隔" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].delayIntv" />
+                </FormItem>
+                <FormItem label="报文封装类型" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].packageType" />
+                </FormItem>
+                <FormItem label="报文播发类型" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].broadcastType" />
+                </FormItem>
+                <FormItem label="延时机制" class="w-[48%]">
+                  <Input v-model:value="portParamState[0].delayMechanism" />
+                </FormItem>
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <Card class="h-100 overflow-auto">
       <Tabs type="card">
         <template v-for="(port, index) in portList" :key="port">
           <TabPane :tab="port">
@@ -59,7 +132,7 @@
           </TabPane>
         </template>
       </Tabs>
-    </Card>
+    </Card> -->
   </Modal>
 </template>
 <script lang="ts" setup>
